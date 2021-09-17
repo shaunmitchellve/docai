ed5954fa9ded1ec7fa307dafe030ad0742a0739f
Shaun Mitchell <shaun@verticalevolution.ca>
2021-09-17 13:27:12 -0600

    FileUpload and view added to front end

--- src/frontendservice/src/components/home/home.jsx
+++ src/frontendservice/src/components/home/home.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles( (theme) => ({
-}));
+//const useStyles = makeStyles( (theme) => ({}));
 
 const Home = () => {
-    const classes = useStyles();
+    //const classes = useStyles();
 
     return (
         <Typography>Welcome Home</Typography>
